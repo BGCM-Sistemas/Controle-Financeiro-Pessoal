caf835fcc82682fdf430cfb657d50ad762d55e99
RafaelMelin33 <rafael.melinsenai@gmail.com>
2025-05-29 11:26:56 -0300

    Criação da carteira e funções de adicionar receitas e despesas
    Criação da páginas html e criação das rotas em python para adicionar receita e despesa

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 app = Flask(__name__)
 app.secret_key = 'senhasupersecretacomçparahackeramericanoerussonaoconseguirinvadir'
 usuarios = []
+receitas = []
+despesas = []
 
 # rota da home
 @app.route('/')
@@ -41,27 +43,27 @@ def cadastro():
                 tem_especial = True
         
         if not tem_maiuscula:
-            flash('A senha deve ter pelo menos uma letra maiúscula!')
+            flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
             return render_template('cadastro.html')
         
         elif not tem_minuscula:
-            flash('A senha deve ter pelo menos uma letra minúscula!')
+            flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
             return render_template('cadastro.html')
         
         elif not tem_numero:
-            flash('A senha deve ter pelo menos um número!')
+            flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
             return render_template('cadastro.html')
         
         elif not tem_especial:
-            flash('A senha deve ter pelo menos um caractere especial!')
+            flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
             return render_template('cadastro.html')
         
         elif len(senha) < 8:
-            flash('A senha deve ter pelo menos 8 caracteres!')
+            flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
             return render_template('cadastro.html')
         
         elif not nome:
-            flash('Nome e sobrenome obrigatório')
+            flash('Nome obrigatório')
             return render_template('cadastro.html')
 
 
@@ -80,7 +82,7 @@ def cadastro():
             "logado": 0 #nao loguei
         }
         usuarios.append(usuario)
-
+        flash('Cadastro realizado com sucesso')
         return redirect(url_for('login'))
 
     else:
@@ -111,6 +113,11 @@ def login():
 # rota de perfil
 @app.route('/perfil')
 def perfil():
+    # aparecer o nome na home
+    nome = ''
+    if len(usuarios) > 0:
+        for usuario in usuarios:
+            nome = usuario["nome"]
     for usuario in usuarios:
         if usuario['logado'] == 1:
             nome = usuario['nome']
@@ -135,13 +142,14 @@ def editar(codigo):
                     usuario['nome'] = request.form['nome']
                     usuario['email'] = request.form['email']
                     usuario['senha'] = request.form['senha']
-# senha forte
+                senha = usuario['senha']
+                # senha forte
                 tem_maiuscula = False
                 tem_minuscula = False
                 tem_numero = False
                 tem_especial = False
 
-                for c in usuario['senha']:
+                for c in senha:
                     if c.isupper(): # se tem maiuscula
                         tem_maiuscula = True
                     elif c.islower(): #se tem minuscula
@@ -152,32 +160,81 @@ def editar(codigo):
                         tem_especial = True
                 
                 if not tem_maiuscula:
-                    flash('A senha deve ter pelo menos uma letra maiúscula!')
+                    flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
                     return render_template('editar.html')
                 
                 elif not tem_minuscula:
-                    flash('A senha deve ter pelo menos uma letra minúscula!')
+                    flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
                     return render_template('editar.html')
                 
                 elif not tem_numero:
-                    flash('A senha deve ter pelo menos um número!')
+                    flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
                     return render_template('editar.html')
                 
                 elif not tem_especial:
-                    flash('A senha deve ter pelo menos um caractere especial!')
+                    flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
                     return render_template('editar.html')
                 
                 elif len(usuario['senha']) < 8:
-                    flash('A senha deve ter pelo menos 8 caracteres!')
+                    flash('A senha deve ter pelo menos 8 dígitos, uma maiúscula, uma minúscula, um número e um caractere especial')
                     return render_template('editar.html')
                 
                 elif not usuario['nome']:
-                    flash('Nome e sobrenome obrigatório')
+                    flash('Nome obrigatório')
                     return render_template('editar.html')
 
         return redirect(url_for('perfil'))
 
     return render_template('editar.html', usuario=usuario)
+
+# Rota da carteira
+@app.route('/carteira')
+def carteira():
+    # aparecer o nome na home
+    # nome = ''
+    # if len(usuarios) > 0:
+    #     for usuario in usuarios:
+    #         nome = usuario["nome"]
+
+    return render_template('carteira.html', receitas=receitas)
+
+@app.route('/adicionar_receita', methods=['GET', 'POST'])
+def adicionar_receita():
+    if request.method == 'POST':
+        valor = request.form['valor']
+        descricao = request.form['descricao']
+        data = request.form['data']
+        codigo = str(len(usuarios) +1)
+        receita = {
+            'valor': valor,
+            'descricao': descricao,
+            'data': data,
+            'codigo': codigo
+        }
+        print(receita)
+        receitas.append(receita)
+        print(receitas)
+        return redirect('/carteira')
+    return render_template('adicionar_receita.html')
+
+@app.route('/adicionar_despesa', methods=['GET', 'POST'])
+def adicionar_despesa():
+    if request.method == 'POST':
+        valor = request.form['valor']
+        descricao = request.form['descricao']
+        data = request.form['data']
+        codigo = str(len(usuarios) +1)
+        despesa = {
+            'valor': valor,
+            'descricao': descricao,
+            'data': data,
+            'codigo': codigo
+        }
+        print(despesa)
+        print(despesas)
+        despesas.append(despesa)
+        return redirect('/carteira')
+    return render_template('adicionar_despesa.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
